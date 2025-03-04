8b47ea1a3028687ebd0c25a797c8f94b5188f7da
rbrock44 <rbrock444@gmail.com>
2025-03-04 12:22:02 -0600

    remove comments

--- Services/TimeAngleService.cs
+++ Services/TimeAngleService.cs
@@ -4,14 +4,12 @@ public class TimeAngleService
 {
     public virtual double Calculate(int hour, int minute)
     {
-        // static hour
         int anglePerHour = 360 / 12;
         int hourAngle = (hour % 12) * anglePerHour;
 
         int anglePerMinute = anglePerHour / 5;
         int minuteAngle = minute * anglePerMinute;
 
-        //in between hours 
         double lastHour = minute * .5;
 
         return hourAngle + minuteAngle + lastHour;
